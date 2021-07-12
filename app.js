372a78cc912aed55b49b7fb32db64a94cc364ea3
GianLucas <gianspf@gmail.com>
2021-07-13 00:54:02 +0200

    GianLucas - NewServer

--- app.js
+++ app.js
@@ -7,6 +7,41 @@ const allPokemon = require("./data");
 
 const app = express();
 
-// -- Define your route listeners here! --
+app.get("/pokemon", (req, res) => {
+  return res.send(allPokemon);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  let id = req.params.id;
+
+  let foundPokemon = allPokemon.find((elem) => {
+    if (typeof elem.id === "number") {
+      return elem.id.toString() === id;
+    }
+    return elem.id === id;
+  });
+
+  if (foundPokemon) {
+    return res.json(foundPokemon);
+  } else {
+    return res.json({ msg: "Pokemon not found" });
+  }
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const queryParams = req.query;
+
+  for (let key in queryParams) {
+    const foundPokemon = allPokemon.find((elem) => {
+      return elem[key].toLowerCase().includes(queryParams[key].toLowerCase());
+    });
+    if (foundPokemon) {
+      return res.json(foundPokemon);
+    } else {
+      return res.json({ msg: "Contact not found." });
+    }
+  }
+  res.json(queryParams);
+});
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
